b37f6321db3f19dd498e3ee4a930ce92c2bb283a
Luana <luanaramosdsantos@gmail.com>
2025-02-05 00:26:41 -0300

    Mudança de cor dos icons, evento copiar email

--- js/script.js
+++ js/script.js
@@ -11,3 +11,29 @@ function verificarScroll(){
 // Adicionando o evento de scroll
 window.addEventListener('scroll', verificarScroll);
 
+
+//Copiar email
+
+// Seleciona o elemento com o ID 'copiarEmail' e adiciona um evento de clique
+document.getElementById('copiarEmail').addEventListener('click', function(event) {
+    
+    // Impede o comportamento padrão do link (evita que ele recarregue a página)
+    event.preventDefault(); 
+
+    // Define a variável 'email' com o endereço de e-mail a ser copiado
+    const email = 'luanaramosdsantos@gmail.com'; 
+
+    // Usa a API do navegador para copiar o texto (neste caso, o e-mail) para a área de transferência
+    navigator.clipboard.writeText(email).then(
+        
+        // Se a cópia for bem-sucedida, exibe um alerta informando o sucesso
+        function() {
+            alert('Email copiado com sucesso!');
+        }, 
+        
+        // Se houver um erro ao copiar, exibe um alerta informando a falha
+        function() {
+            alert('Erro ao copiar email!');
+        }
+    );
+});
